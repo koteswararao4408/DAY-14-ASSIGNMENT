c19d169687e52b0ade0660469946e97f3a5f73dc
koteswararao <koteswararao.pinjala@gmail.com>
2020-12-24 12:36:38 +0530

    Add:Last name of the user validation

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,13 +1,22 @@
 echo "Welcome to User Registration Program"
 #!/bin/bash -x
-#program for first name of the user
+#program for First AND Last name of the user
 
 read -p "Enter a valid first name :" firstName
-pattern="^[[:upper:]]{1}[[:lower:]]{2,}$"	           #pattern for first name of the user
+read -p "Enter a valid last name :" lastName
+pattern1="^[[:upper:]]{1}[[:lower:]]{2,}$"	           #pattern for first name of the user
+pattern2="^[[:upper:]]{1}[[:lower:]]{2,}$"	           #pattern for last name of the user
 
-if [[ $firstName =~ $pattern ]]                            #checking for valid first name
+if [[ $firstName =~ $pattern1 ]]                           #checking for valid first name
 then
     echo "Valid firstname"
 else
     echo "Invalid firstname"
+fi
+
+if [[ $lastName =~ $pattern2 ]]                            #checking for valid lastname
+then
+    echo "Valid lastname"
+else
+    echo "Invalid lastname"
 fi
